b118a2f626c866d3e93c7d6ade816be01e3d0b3b
Carlos Pizarro <kr105@kr105.com>
2023-03-05 15:14:13 -0300

    Added MongoDB testing to Github Actions

--- db.js
+++ db.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const MONGO_USER = process.env.MONGO_USER || 'user';
 const MONGO_PASS = process.env.MONGO_PASS || 'pass';
 const MONGO_HOST = process.env.MONGO_HOST || 'host';
+const MONGO_SRV = (process.env.MONGO_SRV ? '+srv' : '');
 
-const mongoUrl = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/?retryWrites=true&w=majority`;
+const mongoUrl = `mongodb${MONGO_SRV}://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/?retryWrites=true&w=majority`;
 const connect = mongoose.connect(mongoUrl);
 
 connect.then((db) => {
